f60b9db85a57cbaa15966baac397d802fe0ab0e7
okkun_sh <okkun.sh@gmail.com>
2019-04-13 13:50:46 +0900

    added ABC053, 054 a

---
+++ BeginnerContest/ABC053/a.rb
@@ -0,0 +1,2 @@
+a = gets.chomp.to_i
+puts a < 1200 ? "ABC" : "ARC"
---
+++ BeginnerContest/ABC054/a.rb
@@ -0,0 +1,12 @@
+a, b = gets.chomp.split(" ").map(&:to_i)
+if a == b
+  puts "Draw"
+elsif a == 1
+  puts "Alice"
+elsif b == 1
+  puts "Bob"
+elsif a > b
+  puts "Alice"
+elsif b < a
+  puts "Bob"
+end
