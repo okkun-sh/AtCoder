b09065ff2efc48c833a510f6bac9d24b4ca5c39f
okkun-sh <okkun.sh@gmail.com>
2022-12-03 02:13:27 +0900

    feat: ABC278 b

--- BeginnerContest/rust/ABC278/src/bin/b.rs
+++ BeginnerContest/rust/ABC278/src/bin/b.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+       h: u32,
+       m: u32
+    };
+
+    let mut s = h * 60 + m;
+    loop {
+        let h = s / 60 % 24;
+        let m = s % 60;
+        if check(h, m) {
+            println!("{} {}", h, m);
+            return;
+        }
+        s += 1;
+    }
+}
+
+fn check(h: u32, m: u32) -> bool {
+    let h1 = h / 10;
+    let h2 = h % 10;
+    let m1 = m / 10;
+    let m2 = m % 10;
+
+    let ans_h = h1 * 10 + m1;
+    let ans_m = h2 * 10 + m2;
+    ans_h <= 23 && ans_m <= 59
 }
