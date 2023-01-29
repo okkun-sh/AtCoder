c5963c3fcc303e3cb8230f4ca110f7c20beba9c0
okkun-sh <okkun.sh@gmail.com>
2023-01-30 00:05:59 +0900

    feat: add answer b

--- BeginnerContest/rust/ABC280/src/bin/b.rs
+++ BeginnerContest/rust/ABC280/src/bin/b.rs
@@ -1,3 +1,20 @@
+use proconio::{self, input};
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [i32; n]
+    }
+
+    let mut res: Vec<i32> = Vec::new();
+
+    res.push(s[0]);
+    for i in 0..n - 1 {
+        res.push(s[i + 1] - s[i]);
+    }
+
+    let mut ans: Vec<String> = Vec::new();
+    for i2 in res.iter() {
+        ans.push(i2.to_string());
+    }
+    println!("{}", ans.join(" "));
 }
