b828ab361408ec5d4da0f3cb72ba2816cafff43a
okkun-sh <okkun.sh@gmail.com>
2023-01-30 00:41:22 +0900

    feat: add answer c

--- BeginnerContest/rust/ABC280/src/bin/c.rs
+++ BeginnerContest/rust/ABC280/src/bin/c.rs
@@ -1,3 +1,18 @@
+use proconio::{self, input};
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        t: String
+    }
+
+    let s_v: Vec<char> = s.chars().collect();
+    let t_v: Vec<char> = t.chars().collect();
+
+    for i in 0..s_v.len() {
+        if s_v[i] != t_v[i] {
+            println!("{}", i + 1);
+            return;
+        }
+    }
+    println!("{}", t_v.len());
 }
